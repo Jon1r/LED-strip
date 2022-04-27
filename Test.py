360a961238ae2e35ed2e52a764094f5318699693
Jon1r <68221445+Jon1r@users.noreply.github.com>
2022-04-27 18:54:31 +0300

    go

--- Test.py
+++ Test.py
@@ -10,7 +10,7 @@ x = 0
 while y == 1:
     if f == 1:
         f = 0
-        while x < 20:
+        while x <= 20:
             pixels[x] = (255, 0, 0)
             time.sleep(0.5)
             x += 1

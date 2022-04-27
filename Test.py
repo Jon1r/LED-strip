33a934644ef33e6c1ac249ced482f6cae7f4c1f3
Jon1r <68221445+Jon1r@users.noreply.github.com>
2022-04-27 18:58:13 +0300

    go

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ import time
 import board
 import neopixel
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 42)
 
 y = 1
 f = 1
@@ -12,12 +12,13 @@ while y == 1:
         f = 0
         while x <= 20:
             pixels[x] = (255, 0, 0)
-            time.sleep(0.5)
             x += 1
+            time.sleep(0.1)
     else:
         f = 1
         while x > -1:
             pixels[x] = (0, 255, 0)
-            time.sleep(0.5)
             x -= 1
+            time.sleep(0.1)
+
 

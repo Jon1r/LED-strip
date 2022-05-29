7cbe8ba8abace714f4bf7190cf668d359597f356
Jon1r <68221445+Jon1r@users.noreply.github.com>
2022-05-29 12:08:41 +0300

    add new mode

--- StaticColor.py
+++ StaticColor.py
@@ -8,7 +8,7 @@ g = 255
 def staticcolor(k=1, i=0):
         while i < ledsetting.MaxLvl:
                 while k < ledsetting.led_count:
-                        if k=1 or k=3:
+                        if k==1 or k==3:
                                 ledsetting.led_on(i, k, r, g, b)
                                 ledsetting.led_on(i, k+1, r, g, b)
                                 ledsetting.led_on(i, k+2, r, g, b)

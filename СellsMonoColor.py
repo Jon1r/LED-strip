fa000ffd085f998225df89fed400550a350ce381
Jon1r <68221445+Jon1r@users.noreply.github.com>
2022-05-29 20:15:17 +0300

    add new modes

--- СellsMonoColor.py
+++ СellsMonoColor.py
@@ -1,10 +1,10 @@
 import ledsetting
 import time
 
-BrightPar = 10
+BrightPar = 100
 r = 0
 b = 0
-g = 25.5
+g = 2.55
 
 
 def staticcolor(k=1, i=0):
@@ -27,8 +27,8 @@ def staticcolor(k=1, i=0):
 
 def pulsecoloron(k=1, i=0):
         BrightPar = 0
-        while BrightPar <= 10:
-                while i < ledsetting.MaxLvl:
+        while i < ledsetting.MaxLvl:
+                while BrightPar <= 100:
                         while k < ledsetting.led_count:
                                 if k==1 or k==37:
                                         ledsetting.led_on(i, k, r*BrightPar, g*BrightPar, b*BrightPar)
@@ -41,16 +41,17 @@ def pulsecoloron(k=1, i=0):
                                         ledsetting.led_on(i, k+1, r*BrightPar, g*BrightPar, b*BrightPar)
                                         ledsetting.led_on(i, k+2, r*BrightPar, g*BrightPar, b*BrightPar)
                                 k += 7
-                        i += 1
+
                         k = 1
                         time.sleep(0.5)
-                BrightPar += 1
+                        BrightPar += 1
+                i += 1
 
 
 def pulsecoloroff(k=1, i=0):
-        BrightPar = 10
-        while BrightPar >= 0:
-                while i < ledsetting.MaxLvl:
+        BrightPar = 100
+        while i < ledsetting.MaxLvl:
+                while BrightPar >= 0:
                         while k < ledsetting.led_count:
                                 if k == 1 or k == 37:
                                         ledsetting.led_on(i, k, r * BrightPar, g * BrightPar, b * BrightPar)
@@ -63,7 +64,8 @@ def pulsecoloroff(k=1, i=0):
                                         ledsetting.led_on(i, k + 1, r * BrightPar, g * BrightPar, b * BrightPar)
                                         ledsetting.led_on(i, k + 2, r * BrightPar, g * BrightPar, b * BrightPar)
                                 k += 7
-                        i += 1
+
                         k = 1
                         time.sleep(0.5)
-                BrightPar -= 1
+                        BrightPar -= 1
+                i += 1
